c4cf1085132ab54632ac9a1e9365b5128115fd00
Kaique Augusto <kaiquezeza@gmail.com>
2025-11-05 23:13:40 -0300

    ajuste

--- frontend/controle_de_estoque_frontend/vite.config.js
+++ frontend/controle_de_estoque_frontend/vite.config.js
@@ -1,18 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import process from 'node:process'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   envPrefix: 'VITE_',
-  server: {
-    host: true,
-    port: process.env.PORT || 4173
+  build: {
+    outDir: 'dist'
   },
   preview: {
-    host: true,
-    port: process.env.PORT || 4173,
-    allowedHosts: ['https://controle-de-estoque-2-76f9.onrender.com']
+    allowedHosts: ['.onrender.com']
   }
 })
