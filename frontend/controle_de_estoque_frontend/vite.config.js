63ae75d453acebee7661f2d167c3fecc300b833a
Kaique Augusto <kaiquezeza@gmail.com>
2025-11-05 22:51:39 -0300

    ajuste do vite config

--- frontend/controle_de_estoque_frontend/vite.config.js
+++ frontend/controle_de_estoque_frontend/vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import process from 'node:process'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  envPrefix: 'VITE_'
+  envPrefix: 'VITE_',
+  server: {
+    host: true,
+    port: process.env.PORT || 4173
+  },
+  preview: {
+    host: true,
+    port: process.env.PORT || 4173
+  }
 })
